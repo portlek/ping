7023b85a5038bed8530f30846a0917b413e1afa9
portlek <hsndmrts_@outlook.com>
2019-08-02 18:28:39 +0300

    Update Ping.java

--- src/main/java/io/github/portlek/ping/Ping.java
+++ src/main/java/io/github/portlek/ping/Ping.java
@@ -1,5 +1,6 @@
 package io.github.portlek.ping;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -7,14 +8,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Ping extends JavaPlugin implements Listener {
 
+    private FileConfiguration config;
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        this.config = getConfig();
+
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
-    public void messageSend(AsyncPlayerChatEvent event) {
+    public void ping(AsyncPlayerChatEvent event) {
+        if (config == null || event.isCancelled()) return;
+
+        final String pingItem = config.getString("ping-item");
+
+        if (pingItem == null || !event.getMessage().contains(pingItem)) return;
+
+        final String[] split = event.getMessage().split(" ");
 
     }
 
