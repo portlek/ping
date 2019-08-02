53df3c2764603f703777797803c62317b3dbddc8
portlek <hsndmrts_@outlook.com>
2019-08-02 18:21:16 +0300

    config

--- src/main/java/io/github/portlek/ping/Ping.java
+++ src/main/java/io/github/portlek/ping/Ping.java
@@ -1,17 +1,21 @@
 package io.github.portlek.ping;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Ping extends JavaPlugin {
+public final class Ping extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void messageSend(AsyncPlayerChatEvent event) {
 
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
